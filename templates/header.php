1309a5a7ff18d932d1e57eb2131a9fc8ed29d2ec
SOPHA.RATH <sopha.rath@pnc.lan>
2022-03-16 15:02:22 +0700

    test

--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Facebook 2.0</title>
-    <!-- Your style here -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    
+    <div class="navigation w-100 ">
+    <nav class="navbar bg-white d-flex nav">
+        <div class="logo d-flex ml-5">
+            <img src="../images/image 2.png">
+            <form class="form-inline">
+                <input class="form-control ml-2" type="search" placeholder="Search">
+            </form>
+            <div class="icon ml-5 mt-2">
+                <a href=""><li class="fa fa-home"></li></a>
+            </div>
+        </div>
+        <div class="icon">
+            <a href=""><li class="fa fa-user"></li></a>
+        </div>
+        <div class="icon">
+            <a href=""><li class="fas fa-user-friends"></li></a>
+        </div>
+        <div class="login">
+            <button class="btn btn-primary mr-5"><a href=""></a>Login</button>
+        </div>
+    </nav>
+    </div>
+        
--- views/post_view.php
+++ views/post_view.php
@@ -1,3 +1,59 @@
-<div class="container">
-    <!-- Your code here -->
+<?php
+require_once("../templates/header.php");
+?>
+<!-- ---------------------------body-card------------------------------------ -->
+<div class="body-card d-flex justify-content-center">
+  <section class="post post-top">
+    <form action="#">
+      <div class="content">
+        <img src="../images/image1.png"class="logo-p">
+        <div class="details ml-2">
+          <p class="mt-5">Sopha Rath</p>
+          <div class="privacy ml-3">
+            <i class="fas fa-user-friends"></i>
+            <span>Friends</span>
+            <i class="fas fa-caret-down"></i>
+          </div>
+        </div>
+      </div>
+      <textarea placeholder="What's on your mind?" spellcheck="false" required></textarea>
+      <!-- --------------icons--------------------------------------------->
+      <div class="options display-2">
+        <img src="../images/gallery.png" width="9%">
+        <p>Add to Your Post</p>
+      </div>
+      <button class="form-control"><a href=""></a>Post</button>
+    </form>
+  </section>
 </div>
+<!---------------------------container-------------------------------------->
+<div class="container d-flex justify-content-center">
+    <section class="post">
+      <form action="#">
+        <div class="content">
+          <img src="../images/image1.png"class="logo-p">
+          <div class="details ml-2">
+            <p class="mt-5">Sopha Rath</p>
+            <div class="privacy ml-3">
+              <i class="fas fa-user-friends"></i>
+              <span>Friends</span>
+              <i class="fas fa-caret-down"></i>
+            </div>
+          </div>
+        </div>
+         <!---------------------------Result-Message--------------------------------->
+         <p class="p-3"></p>
+        <!-- --------------icons------------------- -->
+        <div class="options display-2">
+           <img src="../images/rady.jpg"class="picture_post">
+        </div>
+        <div class="icons_reach">
+          <a href=""><li class="fa fa-thumbs-up"></li></a>
+          <a href=""><li class="fa fa-comment"></li></a>
+        </div>
+      </form>
+    </section>
+</div>
+<?php
+require_once('../templates/footer.php');
+?>
